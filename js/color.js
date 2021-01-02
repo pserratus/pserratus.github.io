abae66bbbb65907d1d6aee35ea40e053a6882ebf
pserratus <71982764+pserratus@users.noreply.github.com>
2021-01-02 22:37:32 +0100

    Update color.js

--- js/color.js
+++ js/color.js
@@ -1,6 +1,6 @@
 var socket = io('https://wa.mydomotik.ml');
 
       socket.on('color', function(data) {
-        index.querySelectorAll("#bg-primary").style.background = data;
+        index.html.querySelectorAll("#bg-primary").style.background = data;
             console.log(data);
       });
