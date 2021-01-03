312282b9925022c01455a2f5a27dc6eedcda1170
PatrissTV <46244341+PatrissTV@users.noreply.github.com>
2021-01-03 21:42:16 +0100

    Update color.js

--- js/color.js
+++ js/color.js
@@ -1,6 +1,6 @@
 var socket = io('https://wa.mydomotik.ml');
 
       socket.on('color', function(data) {
-        $("#download").css("background-color",data);
+        $(".bg-primary").css("background-color",data);
             console.log(data);
       });
